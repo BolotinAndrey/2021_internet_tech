6f6544238370ee7ab511515197c33d013004bf42
BolotinAndrey <aabolotin98@gmail.com>
2022-01-23 20:33:15 +0300

    Add homework

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+const firstName = 'Водителя'
+const secondName = 'Рейс'
+const dz_name = 'Автобаза'
+const student = 'Болотин А.А.'
+const first_form_first = 'имя Водителя'
+const second_form_first = 'Пункт назначения'
+const second_form_second = 'груз'
+const second_form_third = ''
+const domain = 'http://localhost:8000/api'
+
+export {firstName, secondName, domain, dz_name, student, first_form_first, second_form_second, second_form_third, second_form_first}
